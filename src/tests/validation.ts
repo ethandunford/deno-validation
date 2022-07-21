a933b7e99b90e0d8e86905002773d27e5ecb61d6
Ethan Dunford <ethan.dunford@inchora.com>
2022-07-21 21:37:25 +0100

    isEmpty function, testing and readme update

---
+++ src/tests/validation.ts
@@ -0,0 +1,13 @@
+import { isEmpty } from "../mods.ts";
+import { assertEquals } from "https://deno.land/std@0.149.0/testing/asserts.ts";
+
+Deno.test("isEmpty", () => {
+    assertEquals(isEmpty(""), true);
+    assertEquals(isEmpty("   "), true);
+    assertEquals(isEmpty(null), true);
+    assertEquals(isEmpty(undefined), true);
+    assertEquals(isEmpty(true), false);
+    assertEquals(isEmpty(false), false);
+    assertEquals(isEmpty("Hello World!"), false);
+    assertEquals(isEmpty(0), false);
+});
